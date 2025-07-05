fb067a3a002655d7e69001eaf315e8477bdc3ec5
Venus <122065616+WanYi05@users.noreply.github.com>
2025-07-06 00:03:48 +0800

    Boolean & Operators

---
+++ notes/Boolean & Operators.py
@@ -0,0 +1,36 @@
+# Boolean & Operators!
+
+"""
+Boolean and Operators
+"""
+
+like_coffee = True
+like_tea = False
+
+print(like_coffee)
+
+favorite_food = "Pizza"
+favorite_number = 32
+print(type(like_coffee))
+print(type(favorite_food))
+print(type(favorite_number))
+
+# ---------------------------
+
+# Comparison Operators
+
+print(1 == 2) # False
+print(1 != 2) # True
+print(1 > 2) # False
+print(1 < 2) # True
+print(1 >= 1) # True
+print(1 <= 2) # True
+
+# ---------------------------
+
+# Logical Operators
+
+print(1 > 3 and 5 < 7) # False
+print(1 > 3 or 5 < 7) # True
+print (1 == 1) # True
+print(not (1 == 1)) # False
