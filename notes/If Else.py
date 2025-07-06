84cef739f97e4a613a06d00d825ce1d06cbf2481
Venus <122065616+WanYi05@users.noreply.github.com>
2025-07-07 07:39:46 +0800

    Update If Else.py

--- notes/If Else.py
+++ notes/If Else.py
@@ -1,1 +1,27 @@
 # If Else!
+
+"""
+Flow Control: If Else Elif
+"""
+
+x = 2
+
+if x > 1:
+    print("x is greater than 1")
+
+else: 
+    print("x is not greater than 1")
+
+
+print("Outside of if statement")
+
+# ------------------------------------
+
+hour = 21
+
+if hour < 15:
+    print("Good morning!")
+elif hour < 20:
+    print("Good afternoon!")
+else:
+    print("Good night!")
